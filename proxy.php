c5a5d7dd1f2c0c765e56dd7c83c95b45a3187082
c25 <c@c25.eu>
2014-07-26 11:01:46 +0100

    Initial Commit

---
+++ proxy.php
@@ -0,0 +1,33 @@
+<?php
+
+$site = $_GET['site'];
+$method = $_GET['method'];
+
+if ($site === 'hamqth') {
+	$url = 'http://www.hamqth.com/xml.php?';
+
+	if ($method === 'login') {
+		$url = $url . 'u=' . $_GET['u'] . '&p=' . $_GET['p'];
+	} else if ($method === 'lookup') {
+		$url = $url . 'id='. $_GET['k'] . '&callsign=' . $_GET['call'] . '&prg=qrzqthProxy';
+	}
+} else if ($site === 'qrz') {
+	$url = 'http://xmldata.qrz.com/xml/current/?';
+
+	if ($method === 'login') {
+		$url = $url . 'username=' . $_GET['u'] . ';password=' . $_GET['p'];
+	} else if ($method === 'lookup'){
+		$url = $url . 's=' . $_GET['k'] . ';callsign=' . $_GET['call'];
+
+	}
+}
+
+if ($url !== null) {
+
+	echo(file_get_contents($url));
+
+} else {
+	echo('You did not tell me what to do!');
+}
+
+?>
---
+++ readme.md
@@ -0,0 +1,11 @@
+# QRZ.com & HamQTH API Proxy
+
+Neither QRZ.com nor HamQTH provide an API which can be accessed directly from the web browser (CORS), this is a very simple PHP scrip to proxy some basic API requests to both sites.
+
+## How to use
+
+Very simple, shove the php file somewhere, then call it like so:
+
+```proxy.php?site=qrz&method=login&u=username&p=password```
+
+```proxy.php?site=qth&method=lookup&k=accessKey&call=cllsign```
